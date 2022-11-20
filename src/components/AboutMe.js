fe9dad9b76d87040a047e5eaa28395de5469290b
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-11-19 23:58:19 -0500

    about me rough draft

---
+++ src/components/AboutMe.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div>
+        <h1>Hello, This is Christine Lee.</h1>
+        <h2>I'm a ...</h2>
+        <h3>Dream Catcher</h3>
+        <h3>Problem Solver</h3>
+        <h3>Passionate Learner</h3>
+        <h3>...who's seeking a transition to tech-industry and become a full stack software engineer</h3>
+        <br />
+        <p>In 2022, I took my first step forward to concretize my goal by joining a software engineer training program. Throughout the program, I was able to prepare myself learning how to code and program to develop a website and applications.</p>
+
+        <p>For 2023, I am ready to take another step forward to get into the door.</p>
+        <div>
+            {/* my skill sets */}
+        </div>
+
+    </div>
+  )
+}
